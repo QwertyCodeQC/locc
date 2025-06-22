b8471c717e2b90b2f793b59558f516c79905d15b
QwertyCode <qwerty@qwerty.daxel.pl>
2025-06-22 16:34:50 +0200

    refactor Taskfile: streamline build commands and enhance linting; update main.go to include version display

--- src/main.go
+++ src/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/qwertycodeqc/locc/src/colors"
 )
 
+const VERSION = "1.0"
+
 func main() {
-	fmt.Println(colors.Colorize("locc", colors.Cyan, colors.Bold))
+	fmt.Println(colors.Colorize("locc", colors.Cyan, colors.Bold), "v"+VERSION)
 }
